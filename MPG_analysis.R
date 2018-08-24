42977e6f9f5558d15015ad00a4fb2224ac313095
Matiki <matiasbruzza@gmail.com>
2018-08-24 16:17:52 -0700

    added some exploratory analysis

--- MPG_analysis.R
+++ MPG_analysis.R
@@ -8,3 +8,26 @@ data("mtcars")
 # Take a look at the data
 head(mtcars)
 ?mtcars
+summary(mtcars)
+
+# Preliminary model fit: mpg ~ am
+fit <- lm(mpg ~ am, mtcars)
+fit$coef
+
+# Plot mpg vs am
+ggplot(mtcars,
+       aes(x = am,
+           y = mpg)) +
+        geom_point()
+
+# Look at the mean mpg for cars with/without automatic 
+mtcars %>% group_by(am) %>%
+        summarize(mean = mean(mpg))
+
+# Take another look at the model/coefficients
+summary(fit)
+
+# At first glance it may seem auto transmission is associated with a 7.245
+# increase in mpg, and this is significant with p-value = 0.000285
+
+summary(lm(mpg ~ ., mtcars))
