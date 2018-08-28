3966d48700552afc4c54d5ba439bbd21b5f7810c
Matiki <matiasbruzza@gmail.com>
2018-08-27 17:41:08 -0700

    added GGally, converted columns to factor variables

--- MPG_analysis.R
+++ MPG_analysis.R
@@ -1,6 +1,7 @@
 # Load all necessary R packages into current session
 library(ggplot2)
 library(dplyr)
+library(GGally)
 
 # Read data into R
 data("mtcars")
@@ -10,13 +11,21 @@ head(mtcars)
 ?mtcars
 summary(mtcars)
 str(mtcars)
-# vs & am are binary factor variables
+# It seems like vs & am are binary factor variables
 # cyl, gear, & carb are multi level factor variables
 
+# Convert to factor variables
+mtcars$vs <- as.factor(mtcars$vs)
+mtcars$am <- as.factor(mtcars$am)
+mtcars$cyl <- as.factor(mtcars$cyl)
+mtcars$gear <- as.factor(mtcars$gear)
+mtcars$carb <- as.factor(mtcars$carb)
+
+# Check that the changers took hold
+str(mtcars)
 
 # Preliminary model fit: mpg ~ am
 fit <- lm(mpg ~ am, mtcars)
-fit$coef
 
 # Plot mpg vs am
 ggplot(mtcars,
@@ -34,4 +43,6 @@ summary(fit)
 # At first glance it may seem manual transmission is associated with a 7.245
 # increase in mpg, and this is significant with p-value = 0.000285
 
-summary(lm(mpg ~ ., mtcars))
+# fit regression model with all regressors 
+fit_all <- lm(mpg ~ ., mtcars)
+summary(fit_all)
